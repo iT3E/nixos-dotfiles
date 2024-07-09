fe2ac867d6115c4e8d28fcf769eff275dc48c1bb
iT3E <26583558+iT3E@users.noreply.github.com>
2024-07-08 20:02:38 -0700

    NEW: Remove games

--- modules/home/gaming.nix
+++ modules/home/gaming.nix
@@ -8,21 +8,21 @@
     # inputs.nix-gaming.packages.${pkgs.system}.wine-ge
 
     ## Cli games
-    _2048-in-terminal
-    vitetris
-    nethack
+    # _2048-in-terminal
+    # vitetris
+    # nethack
     
     ## Celeste
-    celeste-classic
-    celeste-classic-pm
+    # celeste-classic
+    # celeste-classic-pm
 
     ## Doom
     # gzdoom
     crispy-doom
 
     ## Emulation
-    sameboy
-    snes9x
+    # sameboy
+    # snes9x
     # cemu
     # dolphin-emu
   ];
