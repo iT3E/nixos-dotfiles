10067b5bf3ab73b916320ec4a97069d00476fbdd
iT3E <26583558+iT3E@users.noreply.github.com>
2024-07-09 16:33:31 -0700

    FIX: Add arch to bootloader

--- modules/core/bootloader.nix
+++ modules/core/bootloader.nix
@@ -1,7 +1,13 @@
-{ pkgs, ... }:
+{ pkgs, host, ... }:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
   boot.kernelPackages = pkgs.linuxPackages_latest;
+  boot.loader.systemd-boot.extraEntries = if (host == "desktop") then {
+    "arch.conf" = ''
+      title Arch
+      efi   /EFI/endeavouros/grubx64.efi
+    '';
+  } else {};
 }
