df2c6f77a8ffec8a357525e030f9acdf7ae82457
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-20 13:31:48 -0700

    IMPROVE: Remove unused pkg and inputs

--- packages/default.nix
+++ packages/default.nix
@@ -5,20 +5,16 @@
   ...
 }:
 {
-  talhelper = inputs'.talhelper.packages.default;
   configure-gtk = pkgs.callPackage ./configure-gtk/default.nix { };
   tokyonight-gtk-theme = pkgs.callPackage ./tokyonight-gtk-theme/default.nix { };
   tokyonight-icon-theme = pkgs.callPackage ./tokyonight-icon-theme/default.nix { };
   nvim-plugins = pkgs.callPackage ./nvim-plugins/default.nix { };
-  fish-plugins = pkgs.fishPlugins.callPackage ./fish-plugins/default.nix { };
-  krr = pkgs.callPackage ./krr/default.nix { };
-  kubectl-rook-ceph = pkgs.callPackage ./kubectl-rook-ceph/default.nix { };
   neovim = inputs'.nixvim.legacyPackages.makeNixvimWithModule {
     # make nixvim use the same pkgs with my overlays added
     inherit pkgs;
     extraSpecialArgs = {
       myPkgs = self'.legacyPackages;
     };
-    module.imports = [ ../home/budiman/config/neovim ];
+    module.imports = [ ../home/it/config/neovim ];
   };
 }
