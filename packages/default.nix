2e6c86f580eadd5ec7f4c7295b2e7d2e590a54a1
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-21 20:06:42 -0700

    FIX: Add system to input

--- packages/default.nix
+++ packages/default.nix
@@ -9,12 +9,12 @@
   tokyonight-gtk-theme = pkgs.callPackage ./tokyonight-gtk-theme/default.nix { };
   tokyonight-icon-theme = pkgs.callPackage ./tokyonight-icon-theme/default.nix { };
   nvim-plugins = pkgs.callPackage ./nvim-plugins/default.nix { };
-  neovim = inputs.nixvim.legacyPackages.makeNixvimWithModule {
+  neovim = inputs.nixvim.legacyPackages.x86_64-linux.makeNixvimWithModule {
     # make nixvim use the same pkgs with my overlays added
     inherit pkgs;
-    extraSpecialArgs = {
-      myPkgs = self.legacyPackages.${system};
-    };
+    # extraSpecialArgs = {
+    #   myPkgs = self.legacyPackages
+    # };
     module.imports = [ ../modules/home/nvim];
   };
 }
