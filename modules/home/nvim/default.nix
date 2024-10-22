bb2e1df2294b5e454e0f760b93a70ef93f508b6f
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-22 16:18:19 -0700

    FIX: Revert home

--- modules/home/nvim/default.nix
+++ modules/home/nvim/default.nix
@@ -1,16 +1,11 @@
-{ pkgs, ... }: 
-let
-  nixvimConfig = {
-    imports = [
-      ./config/plugins.nix
-      ./config/general.nix
-      ./config/appearance.nix
-      ./config/autocmds.nix
-      ./config/keymaps.nix
-    ];
+{
+  imports = [
+    ./config/plugins.nix
+    ./config/general.nix
+    ./config/appearance.nix
+    ./config/autocmds.nix
+    ./config/keymaps.nix
+  ];
 
-    enable = true;
-
-    vim.extraFiles."lua/utils.lua" = builtins.readFile ./config/lua/utils.lua;
-  };
-in nixvimConfig
+  vim.extraFiles."lua/utils.lua" = builtins.readFile ./config/lua/utils.lua;
+}
