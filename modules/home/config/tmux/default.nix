938479dd8e837870aef8ce04bff6d95e718afe9a
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-23 16:04:16 -0700

    IMPROVE: Enable tmux

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -1,21 +1,15 @@
-{
-  config,
-  lib,
-  pkgs,
-  ...
-}:
-let
-  cfg = config.myHome.multiplexer.tmux;
-in
-with lib;
-{
-  options.myHome.multiplexer.tmux = {
-    enable = mkEnableOption "tmux";
-  };
+{ config, lib, pkgs, ... }:
 
-  config = mkIf (cfg.enable) {
-    home.packages = [ pkgs.tmux ];
-    # TODO: use tmux home-manager module when it matures
-    xdg.configFile."tmux".source = ./config;
+{
+  programs.tmux = {
+    enable = true;
+    extraConfig = builtins.readFile ./config/tmux.conf;
+    # Optional but recommended settings
+    terminal = "tmux-256color";
+    clock24 = true;
+    # You can add other tmux settings here if you don't want them in tmux.conf
+    # For example:
+    # shortcut = "a"; # Changes prefix key from C-b to C-a
+    # baseIndex = 1;  # Start windows numbering at 1
   };
 }
