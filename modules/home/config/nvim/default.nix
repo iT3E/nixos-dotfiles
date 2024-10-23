083892c58ba84b1d6f1de4999059d1e7ce02fd42
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-23 11:11:39 -0700

    FIX: Change to _module.args with inherit

--- modules/home/config/nvim/default.nix
+++ modules/home/config/nvim/default.nix
@@ -1,12 +1,15 @@
-{ pkgs, nvim-plugins ? null, ... }:
+{ nvim-plugins ? null, ... }:
 {
   imports = [
-    (import ./config/plugins.nix { inherit nvim-plugins; })
+    ./config/plugins.nix
     ./config/general.nix
     ./config/appearance.nix
     ./config/autocmds.nix
     ./config/keymaps.nix
   ];
+  _module.args = {
+    inherit nvim-plugins;
+  };
   config = {
     extraFiles = {
       "lua/utils.lua" = builtins.readFile ./lua/utils.lua;
