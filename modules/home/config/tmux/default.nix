7cd2e5f90ad8ba70da4eac8a3165a9775a7fe9da
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-23 16:17:27 -0700

    FIX: Theme name

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -10,7 +10,7 @@
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
       {
-        plugin = themepack; 
+        plugin = tmux-themepack; 
         extraConfig = "set -g @themepack 'powerline/default/green'";
       }
       resurrect
