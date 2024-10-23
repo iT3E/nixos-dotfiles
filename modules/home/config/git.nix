7f40998cc85740ec86fca4970663f9dee0bca8e0
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-23 14:39:20 -0700

    NEW: Add delta and gh

--- modules/home/config/git.nix
+++ modules/home/config/git.nix
@@ -9,8 +9,19 @@
     extraConfig = { 
       init.defaultBranch = "main";
       credential.helper = "store";
+      merge.conflictstyle = "diff3";
+      diff.colorMoved = "default";
+    };
+
+    delta = {
+      enable = true;
+      options = {
+        line-numbers = true;
+        side-by-side = true;
+        diff-so-fancy = true;
+        navigate = true;
+      };
     };
   };
-
-  # home.packages = [ pkgs.gh pkgs.git-lfs ];
+  home.packages = [ pkgs.gh ]; # pkgs.git-lfs ];
 }
