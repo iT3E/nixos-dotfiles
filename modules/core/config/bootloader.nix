7fb3eca707e8e1459208789c579d3784f1b40c0e
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-26 16:22:09 -0700

    FIX: Remove typo line

--- modules/core/config/bootloader.nix
+++ modules/core/config/bootloader.nix
@@ -3,7 +3,6 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-    kernelPackages = pkgs.linuxPackages_6_10;
   boot.kernelPackages = pkgs.linuxPackages_6_10;
   boot.loader.systemd-boot.extraEntries = if (host == "desktop") then {
     "arch.conf" = ''
