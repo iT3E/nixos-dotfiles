1148edf987234b9047a156c9f1300c32b204e63e
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-26 13:53:01 -0700

    IMPROVE: Restructure whichkey

--- modules/home/config/nvim/config/keymaps/default.nix
+++ modules/home/config/nvim/config/keymaps/default.nix
@@ -9,55 +9,54 @@
         settings = {
           spec = [
             {
-              __unkeyed-1 = "<leader>w";
-              proxy = "<C-w>";
-              group = "windows";
-            }
-            {
-              __unkeyed-1 = "<c-w>c";
-              desc = "Close current window";
-            }
-            {
-              __unkeyed-1 = "<c-w>H";
-              desc = "Move current window to the far left";
-            }
-            {
-              __unkeyed-1 = "<c-w>J";
-              desc = "Move current window to the very bottom";
-            }
-            {
-              __unkeyed-1 = "<c-w>K";
-              desc = "Move current window to the very top";
-            }
-            {
-              __unkeyed-1 = "<c-w>L";
-              desc = "Move current window to the far right";
-            }
-
-            {
               __unkeyed-1 = "<leader>b";
               group = "buffers";
               desc = "buffers";
             }
 
             {
-              __unkeyed-1 = "<leader>r";
-              group = "refactoring";
-            }
-
-            {
               __unkeyed-1 = "<leader>f";
-              group = "files";
+              group = "file/find";
             }
             {
               __unkeyed-1 = "<leader>u";
-              group = "utils";  
-              desc = "utils";
+              group = "ui";  
+              desc = "ui";
             }
             {
               __unkeyed-1 = "<leader>y";
               group = "yank";  
               desc = "yank";
+            }
+            {
+              __unkeyed-1 = "<leader>g";
+              group = "git";  
+              desc = "git";
+            }
+            {
+              __unkeyed-1 = "<leader>c";
+              group = "code";  
+              desc = "code";
+            }
+            {
+              __unkeyed-1 = "<leader>e";
+              group = "explorer";  
+              desc = "explorer";
+            }
+            {
+              __unkeyed-1 = "<leader>s";
+              group = "search";  
+              desc = "search";
+            }
+            {
+              __unkeyed-1 = "<leader>x";
+              group = "diagnostics";  
+              desc = "diagnostics";
+            }
+            {
+              __unkeyed-1 = "<leader>w";
+              group = "window";  
+              desc = "window";
             }
           ];
           # Using telescope for spelling
@@ -81,46 +80,14 @@
       ## Open new split
       {
         mode = "n";
-        key = "<Leader>s";
+        key = "<Leader>w-";
         action = ":new<CR>";
         options.desc = "Open new horizontal split";
       }
       {
         mode = "n";
-        key = "<Leader>v"; action = ":vnew<CR>";
+        key = "<Leader>w|"; action = ":vnew<CR>";
         options.desc = "Open new vertical split";
-      }
-
-      ## Tab navigation
-      {
-        mode = "n";
-        key = "<Leader>tn";
-        action = ":tabnew<CR>";
-        options.desc = "Create new tab";
-      }
-      {
-        mode = "n";
-        key = "<Leader>tq";
-        action = ":tabclose<CR>";
-        options.desc = "Close current tab";
-      }
-      {
-        mode = "n";
-        key = "<Leader>th";
-        action = ":tabprev<CR>";
-        options.desc = "Go to the previous tab";
-      }
-      {
-        mode = "n";
-        key = "<Leader>tl";
-        action = ":tabnext<CR>";
-        options.desc = "Go to the next tab";
-      }
-      {
-        mode = "n";
-        key = "<Leader>te";
-        action = ":tabedit <C-r>=expand('%:p:h')<CR>/";
-        options.desc = "Open new tab with current buffer's path";
       }
 
       ## Indent or de-indent
@@ -158,56 +125,56 @@
       }
 
       ## FzfLua
-      {
-        mode = "n";
-        key = "<Leader>fz";
-        action = ":FzfLua<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>ff";
-        action = ":FzfLua files<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fg";
-        action = ":FzfLua live_grep<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fc";
-        action = ":FzfLua git_commit<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fb";
-        action = ":FzfLua buffers<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fh";
-        action = ":FzfLua help_tags<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fk";
-        action = ":FzfLua keymaps<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fe";
-        action = ":FzfLua lsp_document_diagnostics<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fr";
-        action = ":FzfLua lsp_references<CR>";
-      }
-      {
-        mode = "n";
-        key = "<Leader>fd";
-        action = ":FzfLua lsp_typedefs<CR>";
-      }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fz";
+      #   action = ":FzfLua<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>ff";
+      #   action = ":FzfLua files<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fg";
+      #   action = ":FzfLua live_grep<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fc";
+      #   action = ":FzfLua git_commit<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fb";
+      #   action = ":FzfLua buffers<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fh";
+      #   action = ":FzfLua help_tags<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fk";
+      #   action = ":FzfLua keymaps<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fe";
+      #   action = ":FzfLua lsp_document_diagnostics<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fr";
+      #   action = ":FzfLua lsp_references<CR>";
+      # }
+      # {
+      #   mode = "n";
+      #   key = "<Leader>fd";
+      #   action = ":FzfLua lsp_typedefs<CR>";
+      # }
 
       ## Trouble
       {
