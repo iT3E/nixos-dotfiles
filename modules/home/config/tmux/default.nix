feb068f89488111e4eb036fee46524d056d94dcb
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-27 08:14:48 -0700

    IMPROVE: Set tmux theme back to default

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -14,28 +14,10 @@
           extraConfig = ''
             set -g @catppuccin_flavor 'mocha'
             set -g @catppuccin_window_status_style "rounded"
-
-            # Status bar icons configuration
-            set -g @catppuccin_status_modules_right "application session date_time"
-            set -g @catppuccin_status_left_separator "█"
-            set -g @catppuccin_status_right_separator "█"
-
-            # For rounded separators, use these instead:
-            # set -g @catppuccin_status_left_separator ""
-            # set -g @catppuccin_status_right_separator ""
-
-            # Center alignment
-            set -g @catppuccin_status_justify "centre"
-
-            # Icon settings
-            set -g @catppuccin_icon_window_last "󰖰"
-            set -g @catppuccin_icon_window_current "󰖯"
-            set -g @catppuccin_icon_window_zoom "󰁌"
-            set -g @catppuccin_icon_window_mark "󰃀"
-            set -g @catppuccin_icon_window_silent "󰂛"
-            set -g @catppuccin_icon_window_activity "󰖲"
-            set -g @catppuccin_icon_window_bell "󰂞"
-          '';
+            set -g status-right-length 100
+            set -g status-left-length 100
+            set -g status-left ""
+            '';
         }
       # {
       #   plugin = power-theme;
