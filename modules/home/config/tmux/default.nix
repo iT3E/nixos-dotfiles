a22abcab8e3cbe9a690a940e0f50e71dc4ccc065
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-27 12:58:03 -0700

    IMPROVE: Add custom resurrect config, add separators

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -21,7 +21,11 @@
             
             # Center the window status
             set -g status-justify centre
-            
+
+            # Set rounded separators
+            set -g @catppuccin_window_left_separator "◖"
+            set -g @catppuccin_window_right_separator "◗"
+
             # Customize window appearance
             set -g @catppuccin_window_default_text "#W"
             set -g @catppuccin_window_current_text "#W"
@@ -31,7 +35,16 @@
             set -g status-right-length 100
           '';
         }
-      resurrect
+      {
+        plugin = resurrect;
+        extraConfig = ''
+          resurrect_dir="$HOME/.tmux/resurrect"
+          set -g @resurrect-dir $resurrect_dir
+          set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
+          set -g @resurrect-capture-pane-contents 'on'
+          set -g @resurrect-processes '"~nvim"'
+        '';
+      }
       {
         plugin = continuum;
         extraConfig = ''
