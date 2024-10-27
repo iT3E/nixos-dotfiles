2c8b8bd97704864b74bfd979ca288d421eb5ba7d
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-27 08:03:15 -0700

    IMPROVE: Move tmux to centered and rounded

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -5,7 +5,7 @@
     enable = true;
     extraConfig = builtins.readFile ./config/tmux.conf;
     terminal = "screen-256color";
-    
+
     # Plugins
     plugins = with pkgs.tmuxPlugins; [
       vim-tmux-navigator
@@ -14,7 +14,28 @@
           extraConfig = ''
             set -g @catppuccin_flavor 'mocha'
             set -g @catppuccin_window_status_style "rounded"
-            '';
+
+            # Status bar icons configuration
+            set -g @catppuccin_status_modules_right "application session date_time"
+            set -g @catppuccin_status_left_separator "█"
+            set -g @catppuccin_status_right_separator "█"
+
+            # For rounded separators, use these instead:
+            # set -g @catppuccin_status_left_separator ""
+            # set -g @catppuccin_status_right_separator ""
+
+            # Center alignment
+            set -g @catppuccin_status_justify "centre"
+
+            # Icon settings
+            set -g @catppuccin_icon_window_last "󰖰"
+            set -g @catppuccin_icon_window_current "󰖯"
+            set -g @catppuccin_icon_window_zoom "󰁌"
+            set -g @catppuccin_icon_window_mark "󰃀"
+            set -g @catppuccin_icon_window_silent "󰂛"
+            set -g @catppuccin_icon_window_activity "󰖲"
+            set -g @catppuccin_icon_window_bell "󰂞"
+          '';
         }
       # {
       #   plugin = power-theme;
