283a761457fc2c3cf1f66997ebd079f39a5aadb1
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-27 13:50:55 -0700

    FIX: Add new resurrect commands

--- modules/home/config/tmux/default.nix
+++ modules/home/config/tmux/default.nix
@@ -14,11 +14,11 @@
           extraConfig = ''
             set -g @catppuccin_flavor 'mocha'
             set -g @catppuccin_window_status_style "rounded"
-            
+
             # Clear the status bar
             set -g status-left ""
             set -g status-right ""
-            
+
             # Center the window status
             set -g status-justify centre
 
@@ -29,7 +29,7 @@
             # Customize window appearance
             set -g @catppuccin_window_default_text "#W"
             set -g @catppuccin_window_current_text "#W"
-            
+
             # Ensure there's enough space
             set -g status-left-length 100
             set -g status-right-length 100
@@ -38,11 +38,9 @@
       {
         plugin = resurrect;
         extraConfig = ''
-          resurrect_dir="$HOME/.tmux/resurrect"
-          set -g @resurrect-dir $resurrect_dir
-          set -g @resurrect-hook-post-save-all "sed -i 's/--cmd lua.*--cmd set packpath/--cmd \"lua/g; s/--cmd set rtp.*\$/\"/' $resurrect_dir/last"
-          set -g @resurrect-capture-pane-contents 'on'
-          set -g @resurrect-processes '"~nvim"'
+            set -g @resurrect-save-command-strategy 'ps'
+            set -g @resurrect-hook-post-save-all 'sed -E "s|:/nix/store/[^ ]+/bin/nvim.*nvim-ruby'\'''([[:space:]]+([^[:space:]]+))?|:nvim \\2|" -i <full-path-to-resurrect-dir>/last'
+            set -g @resurrect-processes '"~nvim"'
         '';
       }
       {
