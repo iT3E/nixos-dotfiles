e12c6a4bbef530e77de3df1c750ecf12ebaca9f6
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-31 21:18:28 -0700

    IMPROVE: Move to linux 6.6 kernel stable

--- modules/core/config/bootloader.nix
+++ modules/core/config/bootloader.nix
@@ -3,11 +3,15 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_6_10;
-  boot.loader.systemd-boot.extraEntries = if (host == "desktop") then {
-    "arch.conf" = ''
-      title Arch
-      efi   /EFI/endeavouros/grubx64.efi
-    '';
-  } else {};
+  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  boot.loader.systemd-boot.extraEntries =
+    if (host == "desktop") then
+      {
+        "arch.conf" = ''
+          title Arch
+          efi   /EFI/endeavouros/grubx64.efi
+        '';
+      }
+    else
+      { };
 }
