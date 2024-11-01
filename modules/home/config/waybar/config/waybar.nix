9621adf26e4c13d412f98ed9775a92e175e98471
iT3E <26583558+iT3E@users.noreply.github.com>
2024-10-31 20:55:28 -0700

    FIX: Disable waybar wireplumber

--- modules/home/config/waybar/config/waybar.nix
+++ modules/home/config/waybar/config/waybar.nix
@@ -1,9 +1,13 @@
-{ pkgs, ... }: 
+{ pkgs, ... }:
 {
   programs.waybar = {
     enable = true;
   };
-  programs.waybar.package = pkgs.waybar.overrideAttrs (oa: {
-    mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
-  });
+  programs.waybar.package =
+    (pkgs.waybar.override {
+      wireplumberSupport = false;
+    }).overrideAttrs
+      (oa: {
+        mesonFlags = (oa.mesonFlags or [ ]) ++ [ "-Dexperimental=true" ];
+      });
 }
