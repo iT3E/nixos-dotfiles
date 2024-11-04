d85167c8901b5bb5e0226bbd9252465a8469ed4d
iT3E <26583558+iT3E@users.noreply.github.com>
2024-11-03 16:19:17 -0800

    IMPROVE: Add logic for different kernels per system

--- modules/core/config/bootloader.nix
+++ modules/core/config/bootloader.nix
@@ -1,9 +1,17 @@
-{ pkgs, host, ... }:
+{
+  pkgs,
+  host,
+  lib,
+  ...
+}:
 {
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.kernelPackages = pkgs.linuxPackages_6_6;
+  boot.kernelPackages = lib.mkMerge [
+    (lib.mkIf (host == "desktop") pkgs.linuxPackages_6_6)
+    (lib.mkIf (host != "desktop") pkgs.linuxPackages_6_6)
+  ];
   boot.loader.systemd-boot.extraEntries =
     if (host == "desktop") then
       {
