0b97d2146c8a2ae2bc0098527b8f401b44c8c98d
iT3E <26583558+iT3E@users.noreply.github.com>
2024-11-03 16:20:46 -0800

    IMPROVE: Add env vars per system

--- modules/home/config/hyprland/config/variables.nix
+++ modules/home/config/hyprland/config/variables.nix
@@ -1,72 +1,72 @@
-{ ... }:
-{
-  home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    __GL_GSYNC_ALLOWED = "0";
-    __GL_VRR_ALLOWED = "0";
-    _JAVA_AWT_WM_NONEREPARENTING = "1";
-    SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
-    DISABLE_QT5_COMPAT = "0";
-    GDK_BACKEND = "wayland";
-    ANKI_WAYLAND = "1";
-    DIRENV_LOG_FORMAT = "";
-    WLR_DRM_NO_ATOMIC = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_QPA_PLATFORM = "xcb";
-    QT_QPA_PLATFORMTHEME = "qt5ct";
-    QT_STYLE_OVERRIDE = "kvantum";
-    MOZ_ENABLE_WAYLAND = "1";
-    WLR_BACKEND = "vulkan";
-    WLR_RENDERER = "vulkan";
-    WLR_NO_HARDWARE_CURSORS = "1";
-    XDG_SESSION_TYPE = "wayland";
-    SDL_VIDEODRIVER = "wayland";
-    CLUTTER_BACKEND = "wayland";
-    GTK_THEME = "Dracula";
-  };
-}
-# { host, lib, ... }:
-# let
-#   # Desktop-specific variables
-#   desktopVariables = {
-#     LIBVA_DRIVER_NAME = "nvidia";
-#     __GLX_VENDOR_LIBRARY_NAME = "nvidia";
-#     WLR_DRM_NO_ATOMIC = "1";
-#   };
-#
-#   laptopVariables = {
-#     # WLR_BACKEND = "vulkan";
-#     # WLR_RENDERER = "vulkan";  
-#     SDL_VIDEODRIVER = "wayland";
-#   };
-#
-#   # Common variables for all hosts
-#   commonVariables = {
+# { ... }:
+# {
+#   home.sessionVariables = {
 #     NIXOS_OZONE_WL = "1";
+#     __GL_GSYNC_ALLOWED = "0";
+#     __GL_VRR_ALLOWED = "0";
 #     _JAVA_AWT_WM_NONEREPARENTING = "1";
 #     SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
 #     DISABLE_QT5_COMPAT = "0";
 #     GDK_BACKEND = "wayland";
 #     ANKI_WAYLAND = "1";
 #     DIRENV_LOG_FORMAT = "";
+#     WLR_DRM_NO_ATOMIC = "1";
 #     QT_AUTO_SCREEN_SCALE_FACTOR = "1";
 #     QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
 #     QT_QPA_PLATFORM = "xcb";
 #     QT_QPA_PLATFORMTHEME = "qt5ct";
 #     QT_STYLE_OVERRIDE = "kvantum";
 #     MOZ_ENABLE_WAYLAND = "1";
+#     WLR_BACKEND = "vulkan";
+#     WLR_RENDERER = "vulkan";
+#     WLR_NO_HARDWARE_CURSORS = "1";
 #     XDG_SESSION_TYPE = "wayland";
+#     SDL_VIDEODRIVER = "wayland";
 #     CLUTTER_BACKEND = "wayland";
 #     GTK_THEME = "Dracula";
-#     __GL_GSYNC_ALLOWED = "0";
-#     __GL_VRR_ALLOWED = "0";
-#     WLR_NO_HARDWARE_CURSORS = "1";
 #   };
-# in
-# {
-#   home.sessionVariables = 
-#     if host == "desktop"
-#     then commonVariables // desktopVariables 
-#     else commonVariables // laptopVariables; 
 # }
+{ host, ... }:
+let
+  # Desktop-specific variables
+  desktopVariables = {
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    WLR_DRM_NO_ATOMIC = "1";
+    GBM_BACKEND = "nvidia-drm";
+    # WLR_NO_HARDWARE_CURSORS = "0";  # deprecated
+  };
+
+  laptopVariables = {
+    SDL_VIDEODRIVER = "wayland";
+  };
+
+  # Common variables for all hosts
+  commonVariables = {
+    NIXOS_OZONE_WL = "1";
+    _JAVA_AWT_WM_NONEREPARENTING = "1";
+    SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
+    DISABLE_QT5_COMPAT = "0";
+    GDK_BACKEND = "wayland";
+    ANKI_WAYLAND = "1";
+    DIRENV_LOG_FORMAT = "";
+    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
+    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
+    QT_QPA_PLATFORM = "xcb";
+    QT_QPA_PLATFORMTHEME = "qt5ct";
+    QT_STYLE_OVERRIDE = "kvantum";
+    MOZ_ENABLE_WAYLAND = "1";
+    XDG_SESSION_TYPE = "wayland";
+    CLUTTER_BACKEND = "wayland";
+    GTK_THEME = "Dracula";
+    __GL_GSYNC_ALLOWED = "0";
+    __GL_VRR_ALLOWED = "0";
+  };
+in
+{
+  home.sessionVariables =
+    if host == "desktop" then
+      commonVariables // desktopVariables
+    else
+      commonVariables // laptopVariables;
+}
