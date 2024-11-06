521dbd35a94c9a1ea8d20f7388260f36e5c2397c
iT3E <26583558+iT3E@users.noreply.github.com>
2024-11-05 21:15:43 -0800

    IMPROVE: Nix fmt

--- modules/home/config/firefox/default.nix
+++ modules/home/config/firefox/default.nix
@@ -1,5 +1,5 @@
 {
-  config,
+  host,
   lib,
   pkgs,
   ...
@@ -120,75 +120,77 @@
           };
         };
 
-        settings = {
-          "accessibility.typeaheadfind.enablesound" = false;
-          "accessibility.typeaheadfind.flashBar" = 0;
-          "browser.aboutConfig.showWarning" = false;
-          "browser.aboutwelcome.enabled" = false;
-          "browser.bookmarks.autoExportHTML" = true;
-          "browser.bookmarks.showMobileBookmarks" = true;
-          "browser.meta_refresh_when_inactive.disabled" = true;
-          "browser.newtabpage.activity-stream.default.sites" = "";
-          "browser.newtabpage.activity-stream.showSponsored" = false;
-          "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
-          "browser.search.hiddenOneOffs" = "Google,Amazon.com,Bing,DuckDuckGo,eBay,Wikipedia (en)";
-          "browser.search.suggest.enabled" = false;
-          "browser.sessionstore.warnOnQuit" = true;
-          "browser.shell.checkDefaultBrowser" = false;
-          "browser.ssb.enabled" = true;
-          "browser.startup.homepage.abouthome_cache.enabled" = true;
-          "browser.startup.page" = 3;
-          "browser.urlbar.keepPanelOpenDuringImeComposition" = true;
-          "browser.urlbar.suggest.quicksuggest.sponsored" = false;
-          "devtools.chrome.enabled" = true;
-          "devtools.debugger.remote-enabled" = true;
-          "dom.storage.next_gen" = true;
-          "dom.forms.autocomplete.formautofill" = true;
-          "extensions.htmlaboutaddons.recommendations.enabled" = false;
-          "extensions.formautofill.addresses.enabled" = false;
-          "extensions.formautofill.creditCards.enabled" = false;
-          "general.autoScroll" = false;
-          "general.smoothScroll.msdPhysics.enabled" = true;
-          "geo.enabled" = false;
-          "geo.provider.use_corelocation" = false;
-          "geo.provider.use_geoclue" = false;
-          "geo.provider.use_gpsd" = false;
-          "gfx.font_rendering.directwrite.bold_simulation" = 2;
-          "gfx.font_rendering.cleartype_params.enhanced_contrast" = 25;
-          "gfx.font_rendering.cleartype_params.force_gdi_classic_for_families" = "";
-          "intl.accept_languages" = "en-US,en";
-          "media.eme.enabled" = true;
-          "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
-          "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
-          "font.name.monospace.x-western" = "Ubuntu Nerd Font";
-          "font.name.sans-serif.x-western" = "Ubuntu Nerd Font";
-          "font.name.serif.x-western" = "Ubuntu Nerd Font";
-          "signon.autofillForms" = false;
-          "signon.firefoxRelay.feature" = "disabled";
-          "signon.generation.enabled" = false;
-          "signon.management.page.breach-alerts.enabled" = false;
-          "xpinstall.signatures.required" = false;
+        settings =
+          {
+            "accessibility.typeaheadfind.enablesound" = false;
+            "accessibility.typeaheadfind.flashBar" = 0;
+            "browser.aboutConfig.showWarning" = false;
+            "browser.aboutwelcome.enabled" = false;
+            "browser.bookmarks.autoExportHTML" = true;
+            "browser.bookmarks.showMobileBookmarks" = true;
+            "browser.meta_refresh_when_inactive.disabled" = true;
+            "browser.newtabpage.activity-stream.default.sites" = "";
+            "browser.newtabpage.activity-stream.showSponsored" = false;
+            "browser.newtabpage.activity-stream.showSponsoredTopSites" = false;
+            "browser.search.hiddenOneOffs" = "Google,Amazon.com,Bing,DuckDuckGo,eBay,Wikipedia (en)";
+            "browser.search.suggest.enabled" = false;
+            "browser.sessionstore.warnOnQuit" = true;
+            "browser.shell.checkDefaultBrowser" = false;
+            "browser.ssb.enabled" = true;
+            "browser.startup.homepage.abouthome_cache.enabled" = true;
+            "browser.startup.page" = 3;
+            "browser.urlbar.keepPanelOpenDuringImeComposition" = true;
+            "browser.urlbar.suggest.quicksuggest.sponsored" = false;
           }
           // lib.optionalAttrs (host == "desktop") {
             "browser.zoom.siteSpecific" = false;
           }
           // {
+            "devtools.chrome.enabled" = true;
+            "devtools.debugger.remote-enabled" = true;
+            "dom.storage.next_gen" = true;
+            "dom.forms.autocomplete.formautofill" = true;
+            "extensions.htmlaboutaddons.recommendations.enabled" = false;
+            "extensions.formautofill.addresses.enabled" = false;
+            "extensions.formautofill.creditCards.enabled" = false;
+            "general.autoScroll" = false;
+            "general.smoothScroll.msdPhysics.enabled" = true;
+            "geo.enabled" = false;
+            "geo.provider.use_corelocation" = false;
+            "geo.provider.use_geoclue" = false;
+            "geo.provider.use_gpsd" = false;
+            "gfx.font_rendering.directwrite.bold_simulation" = 2;
+            "gfx.font_rendering.cleartype_params.enhanced_contrast" = 25;
+            "gfx.font_rendering.cleartype_params.force_gdi_classic_for_families" = "";
+            "intl.accept_languages" = "en-US,en";
+            "media.eme.enabled" = true;
+            "media.videocontrols.picture-in-picture.video-toggle.enabled" = false;
+            "toolkit.legacyUserProfileCustomizations.stylesheets" = true;
+            "font.name.monospace.x-western" = "Ubuntu Nerd Font";
+            "font.name.sans-serif.x-western" = "Ubuntu Nerd Font";
+            "font.name.serif.x-western" = "Ubuntu Nerd Font";
+          }
           // lib.optionalAttrs (host == "desktop") {
             "layout.css.devPixelsPerPx" = 1.5;
           }
           // {
+            "signon.autofillForms" = false;
+            "signon.firefoxRelay.feature" = "disabled";
+            "signon.generation.enabled" = false;
+            "signon.management.page.breach-alerts.enabled" = false;
+            "xpinstall.signatures.required" = false;
 
-          # GPU Acceleration
-          "dom.webgpu.enabled" = true;
-          "gfx.webrender.all" = true;
-          "layers.gpu-process.enabled" = true;
-          "layers.mlgpu.enabled" = true;
+            # GPU Acceleration
+            "dom.webgpu.enabled" = true;
+            "gfx.webrender.all" = true;
+            "layers.gpu-process.enabled" = true;
+            "layers.mlgpu.enabled" = true;
 
-          # Hardware Video Decoding
-          "media.ffmpeg.vaapi.enabled" = true;
-          "media.gpu-process-decoder" = true;
-          "media.hardware-video-decoding.enabled" = true;
-        };
+            # Hardware Video Decoding
+            "media.ffmpeg.vaapi.enabled" = true;
+            "media.gpu-process-decoder" = true;
+            "media.hardware-video-decoding.enabled" = true;
+          };
 
         userChrome = builtins.readFile ./chrome/userChrome.css;
       };
