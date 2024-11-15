7d431076fd9345cf83c8567a47a3b65625bd02f6
iT3E <26583558+iT3E@users.noreply.github.com>
2024-11-14 19:56:19 -0800

    IMPROVE: Add conditional git config

--- modules/home/config/git.nix
+++ modules/home/config/git.nix
@@ -1,16 +1,29 @@
-{ pkgs, ... }:
+{ pkgs, config, ... }:
 {
   programs.git = {
     enable = true;
-
-    userName = "iT3E";
-    userEmail = "26583558+iT3E@users.noreply.github.com";
 
     extraConfig = {
       init.defaultBranch = "main";
       credential.helper = "store";
       merge.conflictstyle = "diff3";
       diff.colorMoved = "default";
+
+      # Personal account - default
+      url."git@github.com:".insteadOf = "https://github.com/";
+
+      # Work account configuration
+      "url \"git@github.com-work:\"".insteadOf = "https://github.com-work/";
+
+      # Conditional includes based on repository path
+      "includeIf \"gitdir:/home/it/git-work/\"" = {
+        path = "/home/it/.config/git/work.gitconfig";
+      };
+
+      # Add personal includeIf
+      "includeIf \"gitdir:/home/it/git-personal/\"" = {
+        path = "/home/it/.config/git/personal.gitconfig";
+      };
     };
 
     delta = {
@@ -23,5 +36,41 @@
       };
     };
   };
+
+  home.activation = {
+    generateWorkGitConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p ~/.config/git
+      cat > ~/.config/git/work.gitconfig << EOF
+      [user]
+        name = $(cat ${config.sops.secrets.git_work_username.path})
+        email = $(cat ${config.sops.secrets.git_work_email.path})
+      EOF
+    '';
+
+    generatePersonalGitConfig = config.lib.dag.entryAfter [ "writeBoundary" ] ''
+      mkdir -p ~/.config/git
+      cat > ~/.config/git/personal.gitconfig << EOF
+      [user]
+        name = iT3E
+        email = 26583558+iT3E@users.noreply.github.com
+      EOF
+    '';
+  };
+
+  # SSH config to handle multiple GitHub accounts
+  home.file.".ssh/config".text = ''
+    # Personal GitHub account
+    Host github.com
+      HostName github.com
+      User git
+      IdentityFile ~/.ssh/id_ed25519_pers
+
+    # Work GitHub account
+    Host github.com-work
+      HostName github.com
+      User git
+      IdentityFile ~/.ssh/id_ed25519_work
+  '';
+
   home.packages = [ pkgs.gh ]; # pkgs.git-lfs ];
 }
--- modules/home/config/sops.nix
+++ modules/home/config/sops.nix
@@ -7,9 +7,19 @@
     defaultSymlinkPath = "/run/user/1000/secrets";
     defaultSecretsMountPoint = "/run/user/1000/secrets.d";
 
-    secrets.openai_api_key = {
-      # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
-      path = "${config.sops.defaultSymlinkPath}/openai_api_key";
+    secrets = {
+      openai_api_key = {
+        # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
+        path = "${config.sops.defaultSymlinkPath}/openai_api_key";
+      };
+      git_work_username = {
+        # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
+        path = "${config.sops.defaultSymlinkPath}/git_work_username";
+      };
+      git_work_email = {
+        # sopsFile = ./secrets.yml.enc; # optionally define per-secret files
+        path = "${config.sops.defaultSymlinkPath}/git_work_email";
+      };
     };
   };
 }
